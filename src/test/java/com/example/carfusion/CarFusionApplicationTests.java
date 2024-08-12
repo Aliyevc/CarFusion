711d392f537e160d4226112d004ccc6290c1bedc
IslamAliyev <islamaliyev888@gmail.com>
2024-08-13 01:12:05 +0400

    project created

---
+++ src/test/java/com/example/carfusion/CarFusionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carfusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarFusionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
